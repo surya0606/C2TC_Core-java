db92ee509715ffb56aba8f6e891d94ad6a5129b1
surya <surya@DESKTOP-0GN24IU>
2023-02-28 11:40:31 +0530

    first commit

---
+++ Day02/src/Prgday2/Breakdemo.java
@@ -0,0 +1,15 @@
+package Prgday2;
+
+public class Breakdemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			if (i==5)
+				break;
+			System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
